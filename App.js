e109e2fdfac47966411e3ef50a8fbdea1de14686
minsa1992 <minsa1992@nate.com>
2019-08-07 18:00:18 +0900

    8.07

--- App.js
+++ App.js
@@ -22,17 +22,34 @@ import {
 const {hegiht, width} = Dimensions.get("window");
 
 export default class App extends React.Component{
+  state = {
+    newToDo: ""
+  };
   render(){
+    const {newToDo} = this.state;
     return(
       <View style={styles.container}>
         <StatusBar barStyle="light-content"/>
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"}/>
+          <TextInput style={styles.input}
+           placeholder={"New To Do"}
+           value={newToDo}
+           onChangeText={this._controlNewToDo}
+           placeholderTextColor={"#999"}
+           returnKeyType={"done"}
+           autoCorrect={false}
+           />
         </View>
       </View>
     );
   }
+
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo : text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -68,5 +85,11 @@ const styles = StyleSheet.create({
           elevation: 3
         }
       })
+    },
+    input:{
+      padding :20,
+      borderBottomColor:"#bbb",
+      borderBottomWidth:1,
+      fontSize:25
     }
 });
